f18e36914e89c426bc780faaa25f1263aa5ac636
clamaveruma <clamaver@uma.es>
2022-03-17 19:20:11 +0100

    jueves

--- src/datos/Datos.java
+++ src/datos/Datos.java
@@ -1,5 +1,27 @@
 package datos;
 
+import java.util.ArrayList;
+
 public class Datos {
 
+	private ArrayList<Double> datos = new ArrayList<>();
+	private ArrayList<String> errores = new ArrayList<>();
+	private double max;
+	private double min;
+	
+	public Datos(String[] cadenas_datos, double min, double max) {
+		// va pasando cada cadena de datos a la lista de numeros
+		
+		for(String cad: cadenas_datos) {
+			try {
+				datos.add(Double.parseDouble(cad));
+			} catch (NumberFormatException e) {	
+				// si hay error de conversión la pasa a la de errores
+				errores.add(cad);
+			}
+		}
+		//guarda max y min
+		this.max = max;
+		this.min = min;
+	}
 }
